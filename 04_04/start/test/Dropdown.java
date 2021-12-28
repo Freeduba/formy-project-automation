f9bd79de6decd64fe9c82a39d53c9b2850aae2eb
Aleksandrs Dubodelovs <freeduba@gmail.com>
2021-12-28 16:19:58 +0200

    Initial commit

---
+++ 04_04/start/test/Dropdown.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Dropdown {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "F:/LinkedIn courses/Selenium Essential Training/chromedriver/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/dropdown");
+
+        WebElement dropDownMenu = driver.findElement(By.id("dropdownMenuButton"));
+        dropDownMenu.click();
+
+        WebElement autocompleteItem = driver.findElement(By.id("autocomplete"));
+        autocompleteItem.click();
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
